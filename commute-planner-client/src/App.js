cb2dc4282a7c54c2c53f5d450e0687fdb6c77829
QED0711 <qdizon@gmail.com>
2019-03-17 14:31:50 -0400

    display routes in select box once loaded from server

--- commute-planner-client/src/App.js
+++ commute-planner-client/src/App.js
@@ -2,25 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// APOLLO
+import { ApolloClient } from 'apollo-boost';
+import { InMemoryCache } from 'apollo-cache-inmemory'; // included with apollo boot
+import { HttpLink } from 'apollo-link-http'; // included with apollo boot
+import {ApolloProvider} from 'react-apollo';
+
+
+// COMPONENTS
+import CurrentRoutes from './components/CurrentRoutes';
+
+
+const client = new ApolloClient({
+    link: new HttpLink({uri: "http://localhost:4000/graphql"}),
+    cache: new InMemoryCache()
+})
+
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <h2>Routes</h2>
+          <CurrentRoutes />
+        </div>
+      </ApolloProvider>
     );
   }
 }
